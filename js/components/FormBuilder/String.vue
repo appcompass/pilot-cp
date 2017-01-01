8b8a36d5cce7f010ac0423a209912646c9cab7b6
Federico Francescato <ffrancescato@gmail.com>
2017-01-01 09:03:42 -0500

    First commit - revamped CMS

---
+++ js/components/FormBuilder/String.vue
@@ -0,0 +1,13 @@
+<template lang="jade">
+input.input(
+  type="text",
+  @input="$emit('input', {value: $event.target.value, pointer: pointer})"
+)
+</template>
+
+<script>
+export default {
+  name: 'string',
+  props: ['pointer', 'data']
+}
+</script>
---
+++ js/components/FormBuilder/Text.vue
@@ -0,0 +1,10 @@
+<template lang="jade">
+textarea.textarea(@input="$emit('input', {value: $event.target.value, pointer: pointer})")
+</template>
+
+<script>
+export default {
+  name: 'Formtext',
+  props: ['pointer', 'data']
+}
+</script>
