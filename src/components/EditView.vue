53b7f658966ef4a7056a12bdcacb85a6b0829dd5
Federico Francescato <ffrancescato@gmail.com>
2017-02-23 16:56:17 -0500

    Started cleaning up Editors

--- src/components/EditView.vue
+++ src/components/EditView.vue
@@ -3,44 +3,42 @@ div(v-if="!loading")
   div(:is="data.edit.editor + 'Editor'", :data="data", :errors="errors", @refresh="refresh")
 
   div.columns
+    .column.is-10
+      footer
+        p.control
+          .pull-right
+            a.button.is-primary(
+              :class="{'is-loading': loading}",
+              :disable="submitted",
+              @click.prevent="update"
+            ) Save
+
+  div.columns
     .column.is-12
       router-view
 
-  footer
-    p.control
-      .pull-right
-        a.button.is-primary(
-          :class="{'is-loading': loading}",
-          :disable="loading",
-          @click="update"
-        ) Save
+
 </template>
 
 <script>
-// import * as Components from './Components'
-// import State from './State'
 import Auth from './Auth'
 import FormEditor from './Editors/FormEditor'
 import MenuEditor from './Editors/MenuEditor'
 
 import swal from 'sweetalert'
-import _ from 'lodash'
+// import _ from 'lodash'
 
 export default {
   name: 'EditView',
   components: { FormEditor, MenuEditor },
   data () {
     return {
-      // Components,
       data: undefined,
-      resource: undefined,
-      edit: {},
-      collection: undefined,
+      submitted: false,
       loading: true,
       model: undefined,
       route: undefined,
       navigation: undefined,
-      // @TODO make errors a computed property?
       errors: []
     }
   },
@@ -56,6 +54,28 @@ export default {
 
   },
   methods: {
+    update () {
+      this.submitted = true
+      this.$http.put(process.env.API_SERVER + this.$route.fullPath.split('_').join('/'), this.data.collection)
+        .then((response) => {
+          this.submitted = false
+          swal({title: 'Success', text: response.data.message, type: 'success'}, () => {
+            this.refresh()
+          })
+        }, response => {
+          this.submitted = false
+          if (response.status === 422) {
+            this.data.edit.fields.forEach((field, index) => {
+              this.errors.push(response.data)
+              if (response.data[field.name]) {
+                this.$set(this.data.edit.fields[index], 'errors', response.data[field.name])
+              }
+            })
+          } else {
+            swal({title: 'Error', text: response.data.errors, type: 'error'})
+          }
+        })
+    },
     refresh () {
       var api = process.env.API_SERVER
       this.loading = true
@@ -69,44 +89,6 @@ export default {
           }
           swal({title: 'Error', text: response.data.errors, type: 'error'})
         })
-    },
-    set (data) {
-      this.$set(this.collection, data.pointer, data.value)
-      if (this.errors != null) {
-        this.clearErrors()
-      }
-    },
-    value (fieldName) {
-      return _.get(this.collection, fieldName)
-    },
-    update () {
-    //   this.loading = true
-    //   this.$http.put(process.env.API_SERVER + this.$route.fullPath.split('_').join('/'), this.collection)
-    //     .then((response) => {
-    //       this.loading = false
-    //       swal({title: 'Success', text: response.data.message, type: 'success'}, () => {
-    //         // fetch a fresh copy of the resource just updated
-    //         this.refresh()
-    //       })
-    //     }, response => {
-    //       this.loading = false
-    //       if (response.status === 422) {
-    //         this.edit.fields.forEach((field, index) => {
-    //           this.errors = response.data
-    //           if (response.data[field.name]) {
-    //             this.$set(this.edit.fields[index], 'errors', response.data[field.name])
-    //           }
-    //         })
-    //       } else {
-    //         swal({title: 'Error', text: response.data.errors, type: 'error'})
-    //       }
-    //     })
-    // },
-    // clearErrors () {
-    //   this.edit.fields.forEach((field, index) => {
-    //     this.$set(this.edit.fields[index], 'errors', null)
-    //   })
-    //   this.errors = null
     }
   }
 }
