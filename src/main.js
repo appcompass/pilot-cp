5121e2bf3feb001b2b3024d0e0f1844542895995
Federico <federico@p3in.com>
2017-03-03 11:37:11 -0500

    Moment directive rebuilds in case the DOM element is moved

--- src/main.js
+++ src/main.js
@@ -40,6 +40,13 @@ Vue.directive('moment-ago', {
       el.innerHTML = ' ' + moment(binding.value, 'YYYY-MM-DD HH:mm:ss').fromNow()
     }, 1000)
   },
+  update (el, binding) {
+    clearInterval(el.interval)
+    el.innerHTML = ' ' + moment(binding.value, 'YYYY-MM-DD HH:mm:ss').fromNow()
+    el.interval = setInterval(() => {
+      el.innerHTML = ' ' + moment(binding.value, 'YYYY-MM-DD HH:mm:ss').fromNow()
+    }, 1000)
+  },
   unbind (el, binding, vnode) {
     clearInterval(el.interval)
   }
