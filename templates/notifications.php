2edc4f6999c259cfff336ee6398b34c8d9f24a5d
Michael Farrell <msfarrel@gmail.com>
2017-03-16 11:34:40 -0400

    header, footer, sidebar, notifications, navigation, users, images

---
+++ templates/notifications.php
@@ -0,0 +1,53 @@
+<div class="notifications-panel">
+	<div class="notifications-header">
+		<h3 class="notifications-heading">Notifications</h3>
+		<a href="" class="notifications-close"><span class="icon icon-cancel"></span></a>
+		<form class="notifications-search">
+			<div class="search-input"><span class="icon-search"></span><input type="search"></span></div>
+		</form>
+	</div>
+	<div class="notifications-category is-active">
+		<h4 class="notifications-category-header">
+			Users <span class="notifications-category-count">3</span> <span class="notifications-category-toggle"></span>
+		</h4>
+		<div class="notifications-content">
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+		</div>
+	</div>
+	<div class="notifications-category">
+		<h4 class="notifications-category-header">
+			Media <span class="notifications-category-count">3</span> <span class="notifications-category-toggle"></span>
+		</h4>
+		<div class="notifications-content">
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+			<div class="notifications-item">
+				<h5 class="notifications-item-type">Login</h5>
+				<time class="notifications-item-time">3 minutes ago</time>
+				<p class="notifications-item-message">John Doe logged in.</p>
+			</div>
+		</div>
+	</div>
+</div>
