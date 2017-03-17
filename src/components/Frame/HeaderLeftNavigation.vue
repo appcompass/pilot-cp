415fd103c8c5ffa458200690ecb676a0fffce3c7
Jubair Saidi <jubair.saidi@gmail.com>
2017-03-17 12:58:11 -0400

    added Frame/ directory containing the official UI's common components (that build the app frame)

---
+++ src/components/Frame/HeaderLeftNavigation.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  ul.header-nav.header-nav-primary
+    li(v-for="cat in navigation.full")
+      router-link(:to="cat.url" exact, v-if="cat.url") {{ cat.title }}
+      a(v-if="!cat.url") {{ cat.title }}
+      ul(v-if="cat.children")
+        li(v-for="item in cat.children")
+          router-link(:to="item.url") {{ item.title }}
+</template>
+
+<script>
+import Navigation from './../State'
+
+export default {
+  name: 'HeaderLeftNavigation',
+  data () {
+    return {
+      navigation: Navigation
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  updated () {
+  }
+}
+</script>
