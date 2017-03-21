41ac800cb9247adbc9b0d32524d09de089fe30b3
Michael Farrell <msfarrel@gmail.com>
2017-03-21 17:03:36 -0400

    page builder

---
+++ templates/sidebar-page-settings.php
@@ -0,0 +1,16 @@
+<div id="sidebar-page-settings" class="sidebar sidebar-page-settings">
+	<form class="page-settings-edit">
+		<div>
+			<label for="">Page Title</label>
+			<input type="text" value="Meet The Team">
+		</div>
+		<div>
+			<label for="">Page Description</label>
+			<textarea>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in pellentesque eros. </textarea>
+		</div>
+		<div>
+			<label for="">Keywords</label>
+			<input type="text" value="Contact Us">
+		</div>
+	</form>
+</div>
