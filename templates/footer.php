43811f4da21b16bad5479d0f89d25a62a0400d70
Michael Farrell <msfarrel@gmail.com>
2017-03-21 09:57:38 -0400

    Overflow/scroll fix. Add Footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,16 @@
-		</div><!-- main-container -->
+				</main><!-- main -->
+				<footer class="footer">
+					<div class="footer-company">
+						<p>Built with <span class="icon-heart"></span> by <a href="">Plus Three Interactive</a>.<br>
+						&copy; <a href="">Plus Three Interactive, LLC</a>. All Rights Reserved.</p>
+						<p class="footer-icons"><a href="" class="link-icon"><span class="icon-github"></span></a></p>
+					</div>
+					<div class="footer-reports">Footer Reports...</div>
+				</footer><!-- footer -->
+			</div><!-- main-container-->
+		</div><!-- content-container -->
 	</div><!-- site-container -->
+	
 	<script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="assets/js/dist/main-min.js"></script>
 </body>
