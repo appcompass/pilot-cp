bedc4ffd4c2dfdc36926e7c8d228476b4ce2e806
Michael Farrell <msfarrel@gmail.com>
2017-03-23 11:47:25 -0400

    Templates: Login, Password Resets, Register

---
+++ templates/register.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pilot-io</title>
+    <link rel="stylesheet" href="assets/css/app-min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700">
+</head>
+<body>
+    
+    <div class="site-logout">
+    	<div class="logout-container">
+    		<div class="logout-logo">
+    			<img src="assets/images/logo_color.svg" width="62" height="80">
+    		</div>
+    		<div class="logout-box">
+    			<p>Please enter the following information to register your account.</p>
+				<form>
+					<label for="">First Name</label>
+					<input type="text">
+					<label for="">Last Name</label>
+					<input type="text">
+					<label for="">Email</label>
+					<input type="email">
+					<label for="">Phone</label>
+					<input type="text">
+					<label for="">Password</label>
+					<input type="password">
+					<label for="">Confirm Password</label>
+					<input type="password">
+					<button class="btn-primary" type="submit">Register</button>
+				</form>
+			</div>
+			<a href="" class="link-text-tertiary link-icon"><span class="icon-arrow-back"></span> Back to Log In</a>
+    	</div>
+    </div>
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/main-min.js"></script>
+</body>
+</html>
