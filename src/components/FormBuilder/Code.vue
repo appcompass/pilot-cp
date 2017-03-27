484fd7b615c7df298c744619107d129910b46f7b
Jubair Saidi <jubair.saidi@gmail.com>
2017-03-27 19:27:19 -0400

    finally got ace (via brace) editor working for code text areas.

--- src/components/FormBuilder/Code.vue
+++ src/components/FormBuilder/Code.vue
@@ -3,38 +3,19 @@ div
   //- @TODO: split the logic up this field should only handle the single field data, not an array of fields on a repeatable.
   p.control
     label.label {{ label }}
-    textarea.textarea(
-      rows="20"
+    editor(
+      :id="elm_id",
       v-if="typeof data === 'string'",
-      v-html="data",
-      @input="set",
-      v-model="data"
+      fontsize="16px",
+      :data="data"
     )
-    //- editor(
-    //-   v-if="typeof data === 'string'",
-    //-   :content="data",
-    //-   lang="javascript",
-    //-   theme="chrome",
-    //-   height="300px",
-    //-   width="100%",
-    //-   sync=false
-    //- )
     span(v-else, v-for="(item, index) in copy")
       span.title.is-6 {{ item.key }}
-      textarea.textarea(
-        rows="20"
-        v-html="item.value",
-        @input="set",
-        v-model="item.value"
+      editor(
+        :id="elm_ids[index]",
+        fontsize="16px",
+        :data="item.value"
       )
-      //- editor(
-      //-   :content="item.value",
-      //-   lang="javascript",
-      //-   theme="chrome",
-      //-   height="300px",
-      //-   width="100%",
-      //-   sync=false
-      //- )
 
     p.help.is-danger
       ul
@@ -46,35 +27,34 @@ div
 
 <script>
 import _ from 'lodash'
-  //- @TODO: get the damn brace (used by vue-ace) module to work, I think it doesn't like webpack. https://github.com/thlorenz/brace/issues/46
-// import editor from 'vue2-ace'
-// import 'brace/mode/javascript'
-// import 'brace/theme/monokai'
+import editor from './Ace'
 
 export default {
   name: 'Code',
   props: ['pointer', 'data', 'label', 'errors', 'help'],
   components: {
-    // editor
+    editor
   },
   data () {
     return {
       elm_id: '',
-      theme: '',
-      mode: '',
+      elm_ids: [],
       copy: []
     }
   },
+  events:{
+  },
   created () {
-    // console.log(this.data)
-    this.elm_id = this.pointer.replace(/\./g, '_')
+    var baseName = this.pointer.replace(/\./g, '_')
+    if (typeof this.data === 'string') {
+      this.elm_id = baseName
+    } else {
+      for (let name in this.data) {
+        this.elm_ids.push(baseName + '_' + name)
+      }
+    }
   },
   mounted () {
-    // var vm = this
-    // vm.$on('editor-update', val => {
-    //   console.log(val)
-    // })
-
     if (_.isEmpty(this.data) && !_.isEmpty(this.source)) {
       if (!_.isEmpty(this.source)) {
         this.source.forEach((item) => {
@@ -100,6 +80,8 @@ export default {
       })
       this.$emit('input', {pointer: this.pointer, value: res})
     }
+  },
+  watch: {
   }
 }
 </script>
