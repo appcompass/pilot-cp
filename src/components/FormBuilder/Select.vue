ecafd4dd3f4b176eb6150ef13ecf86a075164bb4
Federico Francescato <ffrancescato@gmail.com>
2017-04-24 10:55:35 -0400

    minor edit on select

--- src/components/FormBuilder/Select.vue
+++ src/components/FormBuilder/Select.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-  div
-    .select
-      select(@change="set", :value="data", :class="{'is-danger': errors}")
-        option(v-if="field.config.nullable", value="") None
-        option(v-for="option in field.source", :value="option.index") {{ option.label }}
-      span.icon-select
+.select
+  select(@change="set", :value="data", :class="{'is-danger': errors}")
+    option(v-if="field.config.nullable", value="") None
+    option(v-for="option in field.source", :value="option.index") {{ option.label }}
+  span.icon-select
 </template>
 
 <script>
