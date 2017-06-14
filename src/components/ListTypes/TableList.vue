9ab7a0d43a7fc7086f33b1614e6028ae62a8ef08
Federico Francescato <ffrancescato@gmail.com>
2017-06-14 15:00:35 -0400

    Bring back/refactor search/sort, removes parent link, merge password reset request, fixes api.get params)

--- src/components/ListTypes/TableList.vue
+++ src/components/ListTypes/TableList.vue
@@ -1,47 +1,25 @@
 <template lang="pug">
-  div.table-container(
-    :class="{'is-opaque': $parent.loading}"
-  )
+  div.table-container(:class="{'is-opaque': $parent.loading}")
     table.table-default
       thead
         tr
           th
             input(type="checkbox" name="" value="")
-          th(v-for="field in forms.form.fields", nowrap)
-            span.icon-search.table-column-search-trigger(
-              v-if="field.config.searchable",
-              @click="$parent.toggleEdit(field)"
-            )
-            span(
-              @click="$parent.toggleSorter(field)",
-              :class="{'table-sort': field.config.sortable, 'is-active': $parent.sorters[field.name], 'is-reverse': $parent.sorters[field.name] === 'ASC'}"
-            )
-              | {{ field.label }}
+          th(v-for="(field, index) in forms.form.fields", nowrap)
+            span.icon-search.table-column-search-trigger(v-if="field.config.searchable", @click="toggleEdit(field, index)")
+            span(@click="toggleSorter(field)", :class="{'table-sort': field.config.sortable, 'is-active': $parent.sorters[field.name], 'is-reverse': $parent.sorters[field.name] === 'ASC'}") {{ field.label }}
               span.arrow-down
-            form.table-column-search(
-              v-if="field.config.searchable",
-              :class="{'is-active': forms.edit.indexOf(field.id) > -1}"
-            )
+            form.table-column-search(:class="{'is-active': edit.indexOf(field.id) > -1}")
               div.search-input
                 span.icon-search
-                input(
-                  type="search",
-                  v-model="$parent.search[field.name]",
-                  :placeholder="field.label",
-                  @keyup="$parent.applyFilter"
-                )
-                span.icon-cancel(
-                  @click="$parent.toggleEdit(field)"
-                )
+                input(type="search", v-model="search[field.name]", :placeholder="field.label", @keyup="$emit('search', search)", ref="field")
+                span.icon-cancel(@click="toggleEdit(field)")
       tbody(v-if="collection.length")
         tr(v-for="row in collection")
           td
             input(type="checkbox" name="" value="")
           td(v-for="(field, index) in forms.form.fields")
-            span(
-              v-if="index === 0"
-            )
-              //- @TODO: this whole if/else flow is a bit ugly
+            span(v-if="index === 0")
               router-link.table-user-avatar(
                 v-if="row.card_photo && row.abilities.includes('edit')",
                 :to="{name: getRouteName('show'), params: getRouteParams(row.id)}",
@@ -49,9 +27,7 @@
                 img(:src="row.card_photo", width="32", height="32")
               a.table-user-avatar(v-else if="row.card_photo")
                 img(:src="row.card_photo", width="32", height="32")
-              b(
-                v-if="row.abilities.includes('edit')"
-              )
+              b(v-if="row.abilities.includes('edit')")
                 router-link(
                   :to="{name: getRouteName('show'), params: getRouteParams(row.id)}",
                   v-html="value(field.name, row)"
@@ -65,16 +41,10 @@
                 = " | "
                 //- @TODO: add delete ability permissions to resources on API.
                 //- v-if="row.abilities.includes('delete')",
-                a.link-red(
-                  @click="$parent.remove(row.id)"
-                ) Delete
+                a.link-red(@click="$parent.remove(row.id)") Delete
 
-            span(
-              v-if="index > 0",
-              v-html="value(field.name, row)"
-            )
+            span(v-if="index > 0", v-html="value(field.name, row)")
 </template>
-
 <script>
 import _ from 'lodash'
 import RouteHandling from 'Mixins/RouteHandling'
@@ -82,15 +52,49 @@ import RouteHandling from 'Mixins/RouteHandling'
 export default {
   name: 'TableList',
   mixins: [RouteHandling],
-  props: [ 'forms', 'collection' ],
+  props: [ 'forms', 'collection', 'search' ],
   data () {
     return {
+      sorters: {},
+      edit: [],
       _
     }
   },
   methods: {
     value (name, row) {
       return _.get(row, name)
+    },
+    toggleSorter (field, index) {
+      if (!field.config.sortable) {
+        return
+      }
+      let sorter = this.sorters[field.name]
+      switch (sorter) {
+        case 'ASC':
+          this.$set(this.sorters, field.name, 'DESC')
+          break
+        case 'DESC':
+          this.$delete(this.sorters, field.name)
+          break
+        default:
+          this.$set(this.sorters, field.name, 'ASC')
+      }
+      this.$emit('sort', this.sorters)
+    },
+    toggleEdit (field, index) {
+      if (this.edit.indexOf(field.id) > -1) {
+        delete this.search[field.name]
+        this.edit.splice(this.edit.indexOf(field.id), 1)
+        this.$emit('search', this.search)
+      } else {
+        this.edit.push(field.id)
+        if (index) {
+          this.$refs.field[index].focus()
+          setTimeout(() => {
+            this.$refs.field[index].focus()
+          }, 400)
+        }
+      }
     }
   }
 }
