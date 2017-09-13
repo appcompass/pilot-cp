8baf8b2810f120a3f9a64486d548d2ea1f6c1df7
Jubair Saidi <jubair.saidi@p3in.com>
2017-09-13 11:02:54 -0400

    added password field type.

---
+++ src/components/FormFields/Password.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+div
+  .control
+    label.checkbox
+      input(type="checkbox", v-model="edit")
+      |  Change Password
+  .control(v-if="edit")
+    input.input(type="password", v-model="password", placeholder="Password", @input="input", :class="{'is-danger': errors}")
+    input.input(type="password", v-model="password_confirmation", placeholder="Password Again", @input="input", :class="{'is-danger': errors}")
+
+</template>
+
+<script>
+export default {
+  name: 'secret',
+  props: ['errors', 'field'],
+  data () {
+    return {
+      edit: false,
+      password: undefined,
+      password_confirmation: undefined
+    }
+  },
+  methods: {
+    input () {
+        this.$emit('input', {value: this.password, pointer: this.field.name})
+        this.$emit('input', {value: this.password_confirmation, pointer: this.field.name + '_confirmation'})
+    }
+  }
+}
+</script>
