4cc022c0591d7db46c2c05c98709e2bc16f96e54
Federico Francescato <ffrancescato@gmail.com>
2017-11-15 12:26:19 -0500

    user can now select the company he's acting for If the login process returns no current_company information user is prompted with a modal allowing him to select what company he's acting for Auth.js now has an additional `company` construct that keeps track of the selected company. On login if current_company is set we fire the appropriate request (to set current company) and display a modal that shows the currently selected one along with other Options. User Navigation allows to change the company after login (not sure about implication, maybe should refresh the page)

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,4 +1,5 @@
 export const state = state => state.logged
 export const authenticated = state => state.authenticated
 export const user = state => state.user.profile
+export const company = state => state.user.company
 export const abilities = state => state.abilities
