f39875024c8f488dfddd9ff483dbea34d9f057bc
Federico Francescato <ffrancescato@gmail.com>
2017-11-15 12:26:19 -0500

    user can now select the company he's acting for If the login process returns no current_company information user is prompted with a modal allowing him to select what company he's acting for Auth.js now has an additional `company` construct that keeps track of the selected company. On login if current_company is set we fire the appropriate request (to set current company) and display a modal that shows the currently selected one along with other Options. User Navigation allows to change the company after login (not sure about implication, maybe should refresh the page)

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,13 +2,30 @@
 import router from 'src/router'
 import api from '../../../api'
 
-export function ATTEMPT ({commit, state, dispatch}, credentials) {
+export function ATTEMPT ({ commit, state, dispatch }, credentials) {
+  let companies = [
+    { id: '1', name: 'Plus 3 Interactive' },
+    { id: '2', name: 'Triton' }
+  ]
+  dispatch('AVAILABLE_COMPANIES', companies)
+
   return new Promise((resolve, reject) => {
     commit('ATTEMPT', credentials)
-    api.post('/api/auth/login', state.attempt)
-      .then((response) => {
-        let token = `${response.data.data.token_type} ${response.data.data.access_token}`
+    api
+      .post('/api/auth/login', state.attempt)
+      .then(response => {
+        let user = response.data.data.user
+
+        // set api token
+        let token = `${response.data.data.token_type} ${response.data.data
+          .access_token}`
         dispatch('TOKEN', token)
+
+        dispatch('AVAILABLE_COMPANIES', user.companies)
+        if (user.current_company) {
+          dispatch('SET_COMPANY', user.current_company)
+        }
+        dispatch('modal.show', { type: 'Company' })
         commit('LOGIN', true)
         commit('USER', response.data.data.user)
         dispatch('LOGGED')
@@ -21,47 +38,72 @@ export function ATTEMPT ({commit, state, dispatch}, credentials) {
   })
 }
 
-export function CHECK_AUTH ({commit, state, dispatch}, cb) {
+export function CHECK_AUTH ({ commit, state, dispatch }, cb) {
   return new Promise((resolve, reject) => {
     if (!localStorage.hasOwnProperty('token')) {
       commit('LOGIN', false)
-      router.push('/login')
+      // router.push('/login')
       return reject(new Error('Unauthorized'))
     } else {
       dispatch('TOKEN', localStorage.getItem('token'))
     }
-    api.get('/api/auth/user')
+
+    api
+      .get('/api/auth/user')
       .then(response => {
+        let user = response.data.data
+        dispatch('AVAILABLE_COMPANIES', user.companies)
+        if (user.current_company) {
+          dispatch('SET_COMPANY', user.current_company)
+        } else {
+          dispatch('modal.show', { type: 'Company' })
+        }
         commit('LOGIN', true)
         commit('USER', response.data.data)
         dispatch('LOGGED')
         return resolve(true)
-      }, response => {
-        router.push('/login')
+      })
+      .catch(e => {
+        console.log(e)
+        // router.push('/login')
         return resolve(false)
       })
   })
 }
 
-export function LOGOUT ({commit, state, rootState}) {
+export function SET_COMPANY ({ commit, state }, company) {
+  api
+    .post('/api/auth/select-company', { company_id: company.id })
+    .then(r => {
+      console.log(r)
+      commit('SET_COMPANY', r.data.data.current_company)
+    })
+    .catch(e => {
+      commit('ERROR', e)
+    })
+}
+
+export function LOGOUT ({ commit, state, rootState }) {
   commit('LOGOUT')
   router.push('/login')
 }
 
-export function login ({commit, state}, token) {
+export function login ({ commit, state }, token) {
   commit('LOGIN', token)
 }
 
-export function REQUEST_RESET ({commit}, email) {
-  api.post('/api/auth/password/email', {email: email})
-    .then((response) => {
-      console.log(response)
-    })
+export function REQUEST_RESET ({ commit }, email) {
+  api.post('/api/auth/password/email', { email: email }).then(response => {
+    console.log(response)
+  })
 }
 
-export function RESET_PASSWORD ({commit}, payload) {
-  api.post('/api/auth/password/reset', payload)
-    .then(response => {
-      console.log(response)
-    })
+export function AVAILABLE_COMPANIES ({ commit }, availableCompanies) {
+  commit('AVAILABLE_COMPANIES', availableCompanies)
+}
+
+export function RESET_PASSWORD ({ commit }, payload) {
+  api.post('/api/auth/password/reset', payload).then(response => {
+    console.log(response)
+  })
 }
