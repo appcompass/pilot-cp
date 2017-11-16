c4816d6da673b8dc9ee68659f29b45f0602b45f0
Federico Francescato <ffrancescato@gmail.com>
2017-11-16 07:00:54 -0500

    refactors Auth module to a single file, code cleanup

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,49 +1,201 @@
-import * as actions from './auth/actions'
-import * as getters from './auth/getters'
-import * as mutations from './auth/mutations'
+/* global localStorage: false */
+import router from 'src/router'
+import api from '../../api'
+import Abilities from '../../Helpers/Abilities'
 
-class Abilities {
-  constructor () {
-    this.abilities = []
-  }
+/**
+ * State
+ */
 
-  set (abilities) {
-    if (!Array.isArray(abilities)) {
-      console.error('Abilities must be an array.')
+const state = {
+  authenticated: false,
+  abilities: new Abilities(),
+  error: undefined,
+  user: {
+    profile: {
+      gravatar_url: null
+    },
+    company: {
+      current: {},
+      available: []
     }
-    abilities.forEach(ability => {
-      this.abilities.push(ability)
-    })
-  }
-
-  has (ability) {
-    if (!this.abilities) {
-      return false
-    }
-    return this.abilities.includes(ability)
+  },
+  attempt: {
+    email: undefined,
+    password: undefined
   }
 }
 
-export default {
-  state: {
-    authenticated: false,
-    abilities: new Abilities(),
-    error: undefined,
-    user: {
+/**
+ * Actions
+ */
+
+const actions = {
+  ATTEMPT ({ commit, state, dispatch }, credentials) {
+    let companies = [
+      { id: '1', name: 'Plus 3 Interactive' },
+      { id: '2', name: 'Triton' }
+    ]
+    dispatch('AVAILABLE_COMPANIES', companies)
+
+    return new Promise((resolve, reject) => {
+      commit('ATTEMPT', credentials)
+      api
+        .post('/api/auth/login', state.attempt)
+        .then(response => {
+          let user = response.data.data.user
+
+          // set api token
+          let token = `${response.data.data.token_type} ${response.data.data
+            .access_token}`
+          dispatch('TOKEN', token)
+
+          dispatch('AVAILABLE_COMPANIES', user.companies)
+          if (user.current_company) {
+            dispatch('SET_COMPANY', user.current_company)
+          }
+          dispatch('modal.show', { type: 'Company' })
+          commit('LOGIN', true)
+          commit('USER', response.data.data.user)
+          dispatch('LOGGED')
+          return resolve(true)
+        })
+        .catch(error => {
+          commit('LOGIN', false)
+          reject(error)
+        })
+    })
+  },
+
+  CHECK_AUTH ({ commit, state, dispatch }, cb) {
+    return new Promise((resolve, reject) => {
+      if (!localStorage.hasOwnProperty('token')) {
+        commit('LOGIN', false)
+        router.push('/login')
+        return reject(new Error('Unauthorized'))
+      } else {
+        dispatch('TOKEN', localStorage.getItem('token'))
+      }
+
+      api
+        .get('/api/auth/user')
+        .then(response => {
+          let user = response.data.data
+          dispatch('AVAILABLE_COMPANIES', user.companies)
+          if (user.current_company) {
+            dispatch('SET_COMPANY', user.current_company)
+          } else {
+            dispatch('modal.show', { type: 'Company' })
+          }
+          commit('LOGIN', true)
+          commit('USER', response.data.data)
+          dispatch('LOGGED')
+          return resolve(true)
+        })
+        .catch(e => {
+          router.push('/login')
+          return resolve(false)
+        })
+    })
+  },
+
+  SET_COMPANY ({ commit, state }, company) {
+    api
+      .post('/api/auth/select-company', { company_id: company.id })
+      .then(r => {
+        commit('SET_COMPANY', r.data.data.current_company)
+      })
+      .catch(e => {
+        commit('ERROR', e)
+      })
+  },
+
+  LOGOUT ({ commit, state, rootState }) {
+    commit('LOGOUT')
+    router.push('/login')
+  },
+
+  login ({ commit, state }, token) {
+    commit('LOGIN', token)
+  },
+
+  REQUEST_RESET ({ commit }, email) {
+    api.post('/api/auth/password/email', { email: email }).then(response => {
+      console.log(response)
+    })
+  },
+
+  AVAILABLE_COMPANIES ({ commit }, availableCompanies) {
+    commit('AVAILABLE_COMPANIES', availableCompanies)
+  },
+
+  RESET_PASSWORD ({ commit }, payload) {
+    api.post('/api/auth/password/reset', payload).then(response => {
+      console.log(response)
+    })
+  }
+}
+
+/**
+ * Getters
+ */
+
+const getters = {
+  state: state => state.logged,
+  authenticated: state => state.authenticated,
+  user: state => state.user.profile,
+  company: state => state.user.company,
+  abilities: state => state.abilities
+}
+
+/**
+ * Mutations
+ */
+
+const mutations = {
+  ATTEMPT (state, { email, password }) {
+    state.attempt.email = email
+    state.attempt.password = password
+  },
+
+  LOGIN (state, success) {
+    state.attempt = {}
+    state.authenticated = success
+    state.user.authenticated = success
+  },
+
+  LOGOUT (state) {
+    state.user = {
+      authenticated: false,
       profile: {
-        gravatar_url: null
+        gravatar_url: undefined,
+        profile: {
+          gravatar_url: null
+        }
       },
       company: {
         current: {},
         available: []
       }
-    },
-    attempt: {
-      email: undefined,
-      password: undefined
     }
   },
-  getters,
+
+  USER (state, user) {
+    state.user.profile = user
+  },
+
+  SET_COMPANY (state, company) {
+    state.user.company.current = company
+  },
+
+  AVAILABLE_COMPANIES (state, companies) {
+    state.user.company.available = companies
+  }
+}
+
+export default {
+  state,
   actions,
+  getters,
   mutations
 }
