01710b3f8e4e7d6e9511f5fe7a86aef56c3d6f70
Federico Francescato <ffrancescato@gmail.com>
2017-11-16 07:00:54 -0500

    refactors Auth module to a single file, code cleanup

--- src/store/modules/auth/getters.js
+++
@@ -1,5 +0,0 @@
-export const state = state => state.logged
-export const authenticated = state => state.authenticated
-export const user = state => state.user.profile
-export const company = state => state.user.company
-export const abilities = state => state.abilities
